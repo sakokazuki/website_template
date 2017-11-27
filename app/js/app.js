4851aa41c9eceebc64ad5319ac6c21dea9f9861e
sakokazuki <sako@aid-dcc.com>
2017-11-27 16:40:40 +0900

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,36 @@
+import $ from 'jquery'
+import 'babel-polyfill'
+
+class App{
+  constructor(){
+    console.log("app ready");
+
+    console.log("env: "+ENV.env);
+    this.sequence()
+  }
+
+  async sequence(){
+    console.log("--- 0 ---")
+    await this.asyncFunc(1000)
+    console.log("--- 1 ---")
+    await this.asyncFunc(1000)
+    console.log("--- 2 ---")
+    await this.asyncFunc(1000)
+  }
+
+  async asyncFunc(time){
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        console.log("wait "+time)
+        resolve();
+      }, time)
+    });
+  }
+
+}
+
+$(document).ready(()=>{
+  const app = new App();
+});
+
+
